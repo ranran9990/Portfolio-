af02be15029ab437b23545379dc295fd3f86daca
ranran9990 <randywh27@gmail.com>
2025-08-21 12:13:48 -0700

    Delete lab3 directory

--- lab3/source/edge_dual.sv
+++
@@ -1,8 +0,0 @@
-module edge_dual(
-  input logic clk, n_rst, async_in,
-  output logic sync_out, edge_flag
-);
-
-  edge_det #(.RST_VAL(1'b0), .TRIG_RISE(1'b1), .TRIG_FALL(1'b1)) dual (.*);
-  
-endmodule
